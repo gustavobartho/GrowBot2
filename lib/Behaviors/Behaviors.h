898d05d12bc89a99b79f27e57c92a3ba0d2a7739
gustavobartho <gustavobtrad@gmail.com>
2025-10-06 08:56:30 -0300

    reverted to working code

--- lib/Behaviors/Behaviors.h
+++
@@ -1,9 +0,0 @@
-#ifndef Behaviors_h
-#define Behaviors_h
-
-#include "Behavior.h"
-#include "OnOffBehavior.h"
-#include "StepBehavior.h"
-#include "InputBoundBehavior.h"
-
-#endif
--- lib/Behaviors/src/Behaviors.h
+++
@@ -1,7 +0,0 @@
-#ifndef Behaviors_h
-#define Behaviors_h
-
-#include "OnOffBehavior.h"
-#include "StepBehavior.h"
-
-#endif
